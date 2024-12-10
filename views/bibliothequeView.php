4136c290b6f07693a595d8f3ac69d92525fb5ee8
Dogastuj <lenourichel.nathan@gmail.com>
2024-12-10 10:41:14 +0100

    Create bibliothequeView.php
    création de bibliothèqueView + création du dossier css

---
+++ views/bibliothequeView.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bibliothèque</title>
+</head>
+<body>
+    <div class="navBar">
+    <ul id="navBar">
+                    <li><a class="logo"href="index.php"><img src="../ressources/logo.png" alt="Logo"></a></li>
+                            <li id="rightSide">
+                                <a>
+                                <li><a id="pad" href="bibliothèqueview.php"><u>Ma Bibliothèque</u></a></li>
+                                <li><a id="pad" href="ajouterUnJeu.php">Ajouter un jeu</a></li>
+                                <li><a id="pad" href="classement.php">Classement</a></li>
+                                <li><a id="pad" href="profil.php">Profil</a></li>
+                                </a>
+                            </li>
+                </ul>
+    </div>
+</body>
+</html>
