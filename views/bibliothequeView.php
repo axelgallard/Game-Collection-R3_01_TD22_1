bc885f38e2bda0f5db5af23035f7fd92c381256e
Dogastuj <lenourichel.nathan@gmail.com>
2024-12-10 10:48:24 +0100

    ajout du dossier ressources

--- views/bibliothequeView.php
+++ views/bibliothequeView.php
@@ -10,12 +10,12 @@
     <ul id="navBar">
                     <li><a class="logo"href="index.php"><img src="../ressources/logo.png" alt="Logo"></a></li>
                             <li id="rightSide">
-                                <a>
+                              
                                 <li><a id="pad" href="bibliothèqueview.php"><u>Ma Bibliothèque</u></a></li>
                                 <li><a id="pad" href="ajouterUnJeu.php">Ajouter un jeu</a></li>
                                 <li><a id="pad" href="classement.php">Classement</a></li>
                                 <li><a id="pad" href="profil.php">Profil</a></li>
-                                </a>
+                               
                             </li>
                 </ul>
     </div>
