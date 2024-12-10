fdecad8e11349e177c8127d3f052a3fecf61cd4b
axelgallard <90622695+axelgallard@users.noreply.github.com>
2024-12-10 10:05:02 +0100

    modification de lu model pour récup les valeurs dans le .env

--- models/model.php
+++ models/model.php
@@ -3,10 +3,12 @@ class ModelArticle{
     private $db;
 
     public function __construct() {
-        $hostname = 'localhost'; 
-        $dbname = 'td_final'; 
-        $username = 'root'; 
-        $password=null;
+        $env = parse_ini_file('.env', true);
+
+        $hostname =  $env['DB_HOST'];
+        $dbname = $env['DB_NAME'];
+        $username = $env['DB_USER'];
+        $password = $env['DB_PASSWORD'];
 
         $this->db = new PDO("mysql:host=$hostname;dbname=$dbname;charset=utf8;", $username, $password);
     }
