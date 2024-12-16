a778385daa67f9f1c57fb3b36f63eed4ffa68621
FireDarkRP-PRO <johnny.granger.pro@gmail.com>
2024-12-16 16:27:39 +0100

    correction du chemain d'accès
    UwU - correction du chemain d'accès

--- views/bibliotheque.php
+++ views/bibliotheque.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="css/navbar.css">
     <title>Bibliothèque</title>
 </head>
 <body>
     <div class="navBar">
         <ul id="navBar">
-                        <a href="bibliotheque.php"><img src="../ressources/logo.png" alt="Logo" id="logo" ></a>
+                        <a href="bibliotheque.php"><img src="ressources/logo.png" alt="Logo" id="logo" ></a>
 
                         <li id="rightSide"><a id="pad" href="profil.php">Profil</a></li> 
                         <li id="rightSide"><a id="pad" href="classement.php">Classement</a></li>
