c7943374178710b251ac7c14c13b5028c7b6b811
axelgallard <axelgallard72@gmail.com>
2024-12-16 17:42:59 +0100

    légère modif (je sais plus ce que c'est mais faut que je push askip)

--- views/bibliotheque.php
+++ views/bibliotheque.php
@@ -9,12 +9,12 @@
 <body>
     <div class="navBar">
         <ul id="navBar">
-                        <a href=""><img src="ressources/logo.png" alt="Logo" id="logo" ></a>
+            <a href=""><img src="ressources/logo.png" alt="Logo" id="logo" ></a>
 
-                        <li id="rightSide"><a id="pad" href="profil.php">Profil</a></li> 
-                        <li id="rightSide"><a id="pad" href="classement.php">Classement</a></li>
-                        <li id="rightSide"><a id="pad" href="ajouterUnJeu.php">Ajouter un jeu</a></li>
-                        <li id="rightSide"><a id="pad" href="bibliotheque.php"><u>Ma Bibliothèque</u></a></li>   
+            <li id="rightSide"><a id="pad" href="profil.php">Profil</a></li> 
+            <li id="rightSide"><a id="pad" href="classement.php">Classement</a></li>
+            <li id="rightSide"><a id="pad" href="ajouterUnJeu.php">Ajouter un jeu</a></li>
+            <li id="rightSide"><a id="pad" href="bibliotheque.php"><u>Ma Bibliothèque</u></a></li>   
         </ul>
     </div>
 
