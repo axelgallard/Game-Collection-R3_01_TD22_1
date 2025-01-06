68d41b13927a99f595780d741512b5f213eebd3b
Dogastuj <lenourichel.nathan@gmail.com>
2025-01-06 15:30:10 +0100

    navbar.php a mettre dans toute les pages

--- views/bibliotheque.php
+++ views/bibliotheque.php
@@ -7,18 +7,9 @@
     <title>Bibliothèque</title>
 </head>
 <body>
-    <div class="navBar">
-        <ul id="navBar">
-            <a href=""><img src="ressources/logo.png" alt="Logo" id="logo" ></a>
+    <?php require_once('navBar.php') ?>
 
-            <li id="rightSide"><a id="pad" href="profil.php">Profil</a></li> 
-            <li id="rightSide"><a id="pad" href="classement.php">Classement</a></li>
-            <li id="rightSide"><a id="pad" href="ajouterUnJeu.php">Ajouter un jeu</a></li>
-            <li id="rightSide"><a id="pad" href="bibliotheque.php"><u>Ma Bibliothèque</u></a></li>   
-        </ul>
-    </div>
-
-    <div class="zoneImg">
+    <div id="zoneImg">
         <h1>Salut <?php echo $prenom ?> ! <br>
         PRÊT À AJOUTER DES JEUX A TA COLLECTION ?
         </h1>
