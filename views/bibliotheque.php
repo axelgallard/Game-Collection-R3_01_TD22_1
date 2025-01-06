b089d8b7e8c67cea4b443d772aa522f2f2ceef00
Dogastuj <lenourichel.nathan@gmail.com>
2025-01-06 15:59:55 +0100

    bcp de trucs flm d'expliquer

--- views/bibliotheque.php
+++ views/bibliotheque.php
@@ -4,15 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/bibliotheque.css">
     <title>Bibliothèque</title>
 </head>
 <body>
     <?php require_once('navBar.php') ?>
 
     <div id="zoneImg">
-        <h1>Salut <?php echo $prenom ?> ! <br>
+        <h1>Salut <?php // echo $prenom ?> ! <br>
         PRÊT À AJOUTER DES JEUX A TA COLLECTION ?
         </h1>
+    </div>
+
+    <br>
+    <h2>Mes jeux</h2>
+
+    <div id="#gamesDisplayArea">
+
+        <?php //displayLibraryGames() ?>
+
     </div>
 
 </body>
