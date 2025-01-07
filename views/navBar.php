dd87541bd6898feb6b54b718a16cee84014d89e0
Dogastuj <lenourichel.nathan@gmail.com>
2025-01-07 13:17:21 +0100

    passage des titres de la navbar en majuscule pour coller avec la maquette

--- views/navBar.php
+++ views/navBar.php
@@ -5,25 +5,25 @@
             <li id="rightSide">
                 <form method="post" action="">
                     <input type="hidden" name="page" value="profil">
-                    <button type="submit" id="pad">Profil</button>
+                    <button type="submit" id="pad">PROFIL</button>
                 </form>
             </li>
             <li id="rightSide">
                 <form method="post" action="">
                     <input type="hidden" name="page" value="classement">
-                    <button type="submit" id="pad">Classement</button>
+                    <button type="submit" id="pad">CLASSEMENT</button>
                 </form>
             </li>
             <li id="rightSide">
                 <form method="post" action="">
                     <input type="hidden" name="page" value="listeJeux">
-                    <button type="submit" id="pad">Ajouter un jeu</button>
+                    <button type="submit" id="pad">AJOUTER UN JEU</button>
                 </form>
             </li>
             <li id="rightSide">
                 <form method="post" action="">
                     <input type="hidden" name="page" value="bibliotheque">
-                    <button type="submit" id="pad">Ma Bibliothèque</button>
+                    <button type="submit" id="pad">MA BIBLIOTÈQUE</button>
                 </form>
             </li>
         </ul>
