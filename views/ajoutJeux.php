8984c5f5f35a7584962d41f1700e57cc9724f7e6
Dogastuj <lenourichel.nathan@gmail.com>
2025-01-07 12:43:11 +0100

    imputs en noir + css de la liste des jeux

--- views/ajoutJeux.php
+++ views/ajoutJeux.php
@@ -2,12 +2,13 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/navbar.css">
+        <link rel="stylesheet" href="css/ajoutJeux.css">
         <title>Ajouter un Jeu</title>
     </head>
     <body>
         <!-- NAVBAR -->
         <?php require_once('navBar.php') ?>
-        <!--        -->
+        <!---->
         <h1>Ajouter un jeu a sa bibliothèque</h1>
         <form action="" method="POST">
             <input type="text" id="Recherche" name="Recherche" value="" placeholder="Rechercher un jeu" require>
@@ -17,10 +18,12 @@
         </form>
 
         <h2>Resultats de la recherche</h2>
+
+        <div id="gamesDisplayArea">
             <?php 
                 foreach($gamesList as $aGame){
                     ?>
-                    <div class="game" style="background-image: url('<?php echo $aGame->getUrlCover() ?>')">
+                    <div class="aGame" style="background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 100%), url('<?php echo $aGame->getUrlCover() ?>');">
                         <form action="" method="POST">
                             <h3><?php echo $aGame->getNomJeu() ?></h3>
                             <p id="plateforme"><?php echo $aGame->getPlateformes() ?></p>
@@ -33,11 +36,6 @@
                 <?php 
                 }
             ?>
-        <h2>Vous ne trouvez pas votre bonheur ?</h2>
-        <form action="" method="POST">
-            <button type="submit">Ajouter un Nouveau jeu</button>
-            <input type="hidden" id="AjoutForm" name="AjoutForm" value="AjoutForm">
-            <input type="hidden" id="page" name="page" value="listeJeux">
-        </form>
+        </div>
         
     </body>
